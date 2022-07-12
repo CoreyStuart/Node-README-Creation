831720de93303feb96d9a52feb52623060338d4c
Corey Stuart <c.stuart21@yahoo.com>
2022-07-12 13:02:20 -0700

    finished product

--- index.js
+++ index.js
@@ -2,84 +2,91 @@
 
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 
 // TODO: Create an array of questions for user input
-    inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: 'What is your github?',
-      name: 'username',
-    },
 
-    {
-        type: 'input',
-        message: 'What is your email address?',
-        name: 'email',
-    },
 
-    {
-        type: 'input',
-        message: 'Whats your projects name?' ,
-        name: 'project',
-    },
 
-    {
-        type: 'input',
-        message: 'Please have a short description of your project:' ,
-        name: 'descrition',
-    },
-
-    {
-      type: 'list',
-      message: 'What kind of license should your project have?',
-      choices: ['MIT', 'ISC', 'Mozilla Public License 2.0', 'LaTeX Project Public License v1.3c'],
-      name: 'license',
-    },
-
-    {
-      type: 'list',
-      message: 'What command should be run to install dependencies?',
-      choices: ['npm i'],
-      name: 'dependencies',
-    },
-
-    {
-        type: 'list',
-        message: 'What command should be run to run tests?',
-        choices: ['npm test'],
-        name: 'test',
-    },
-
-    {
-        type: 'input',
-        message: 'What does the user need to know about using the repo?',
-        name: 'using',
-    },
-
-    {
-        type: 'input',
-        messsage: 'What does the user need to know about contributing to the repo?',
-        name: 'contributing',
-    }
-
-  ]) .then((response) =>
-  response.confirm === response.password
-    ? console.log('Success!')
-    : console.log('You forgot your password already?!')
-);
 // TODO: Create a function to write README file
 
 
 
 // let data = questions.name;
-// function writeToFile(fileName, data) {
-//     fs.writeFile('README.md', data, utf8, (err))
-// }
+ function writeToFile(fileName, data) {
+     const response = generateMarkdown(data);
+   fs.writeFile('README.md', response, 'utf8', (err) => 
+   err ? console.error(err) : console.log('Success!'))
+ };
+
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { 
+    inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: 'What is your github?',
+        name: 'username',
+      },
+
+      {
+          type: 'input',
+          message: 'What is your email address?',
+          name: 'email',
+      },
+  
+      {
+          type: 'input',
+          message: 'Whats your projects name?' ,
+          name: 'title',
+      },
+  
+      {
+          type: 'input',
+          message: 'Please have a short description of your project:' ,
+          name: 'description',
+      },
+  
+      {
+        type: 'list',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'ISC', 'Mozilla Public License 2.0', 'LaTeX Project Public License v1.3c'],
+        name: 'license',
+      },
+  
+      {
+        type: 'input',
+        message: 'What command should be run to install dependencies?',
+        default: 'npm i',
+        name: 'dependencies',
+      },
+  
+      {
+          type: 'input',
+          message: 'What command should be run to run tests?',
+          default: 'npm test',
+          name: 'test',
+      },
+  
+      {
+          type: 'input',
+          message: 'What does the user need to know about using the repo?',
+          name: 'using',
+      },
+  
+      {
+          type: 'input',
+          messsage: 'What does the user need to know about contributing to the repo?',
+          name: 'contributing',
+      }
+  
+    ]).then((response) => {
+        writeToFile('README.md', response);
+    });
+}
 
 // Function call to initialize app
 init();
